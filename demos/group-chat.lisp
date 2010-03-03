77390b7b6f60fcf312ed311b4ffa78c41e6731d5
Ted Roden <tedroden@gmail.com>
2010-03-03 11:17:39 -0500

    use less typing by including an 'in-package' call

--- demos/group-chat.lisp
+++ demos/group-chat.lisp
@@ -5,7 +5,7 @@
 ;;
 
 (require "http-server.lisp" "../http-server.lisp")
-
+(in-package :http-server)
 (defvar *listeners* (make-array 0
 								:element-type 'standard-class
 								:fill-pointer 0
@@ -32,30 +32,30 @@
 	  (loop for i from 1 to (fill-pointer *listeners*)
 		 do (let ((x (vector-pop *listeners*)))
 			  ;; send the message to each of of them
-			  (http-server::finish-http-response x message-body))))
+			  (finish-http-response x message-body))))
 
 	;; response with the message... probably ignore by the client
-	(http-server::finish-http-response conn message-body)))
+	(finish-http-response conn message-body)))
 
 
 
 (defun handle-send (conn) 
   (send-message 
    conn
-   (gethash "from" (slot-value conn 'http-server::get-params))
-   (gethash "body" (slot-value conn 'http-server::get-params))))
+   (gethash "from" (slot-value conn 'get-params))
+   (gethash "body" (slot-value conn 'get-params))))
 
 (defun handle-404 (conn)
-  (http-server::finish-http-response conn "404!"))
+  (finish-http-response conn "404!"))
 
 (defun handle-static-js (conn)
-  (http-server::finish-static-file conn "static/group-chat.js"))
+  (finish-static-file conn "static/group-chat.js"))
 
 (defun handle-static-home (conn)
-  (http-server::finish-static-file conn "static/group-chat.html"))
+  (finish-static-file conn "static/group-chat.html"))
 
 (defun handle-static-css (conn)
-  (http-server::finish-static-file conn "static/group-chat.css"))
+  (finish-static-file conn "static/group-chat.css"))
 
 (defun handle-request (conn) 
   (let ((handled nil))
@@ -63,7 +63,7 @@
 	;; loop through the URL handlers
 	(dolist (map *url-handlers*)
 	  ;; when we find the url mapping call the function
-	  (when (eq 0 (search (car map) (slot-value conn 'http-server::request-uri)))
+	  (when (eq 0 (search (car map) (slot-value conn 'request-uri)))
 
 		;; fixme: there must be a better way to get the last/second item 
 		;; (cdr returns the rest)
@@ -78,4 +78,4 @@
 			  (handle-404 conn))))
 
 ;; setup the server and our handler
-(http-server::http-server :request-handler 'handle-request)
+(http-server :request-handler 'handle-request)
--- demos/hello-world.lisp
+++ demos/hello-world.lisp
@@ -3,12 +3,13 @@
 ;; then point your browser to http://localhost:9021
 
 (require "http-server.lisp" "../http-server.lisp")
+(in-package :http-server)
 
 ;; handle the HTTP request
 (defun handle-request (conn) 
   ;; show "Hello Visitor #1" where 1 is the nth connection
-  (http-server::finish-http-response conn (format nil "<h1>Hello Visitor #~a</h1>"
-												  (slot-value conn 'http-server::id))))
+  (finish-http-response conn (format nil "<h1>Hello Visitor #~a</h1>"
+									 (slot-value conn 'id))))
 
 ;; start the server
-(http-server::http-server :request-handler 'handle-request) 
+(http-server :request-handler 'handle-request) 
