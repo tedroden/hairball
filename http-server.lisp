916c14acf2ef156fcf1bda371bec14d207690eda
Ted Roden <troden@knuckles.local>
2010-03-13 22:35:26 -0500

    about to break some stuff, already broken, just checking in my progress.

--- http-server.lisp
+++ http-server.lisp
@@ -4,8 +4,14 @@
 (defvar *default-http-port* 9021)
 (defvar *socket-listen-ip* #(127 0 0 1))
 (defvar *socket-listen-backlog* 500)
-
+(defvar *read-buffer-size* 8)
 (defvar *octet* '(unsigned-byte 8))
+
+;; ;; from hunchentoot
+;; (defvar *crlf*
+;;   (make-array 2 :element-type '(unsigned-byte 8)
+;;               :initial-contents (mapcar 'char-code '(#\Return #\Linefeed))))
+
 
 (defparameter *http-server-version* "0.0.1")
 
@@ -111,69 +117,167 @@
 
 
 ;; this if hte first http responder. we want to wait for the theaders
+;; (defun make-http-responder (conn)
+;;   (lambda (_)
+;;     (declare (ignore _))
+;;     (handler-case
+;; 		(loop
+;; 		  (let ((line (read-line (slot-value conn 'stream)))
+;; 				(in-body nil))
+			
+;; 			;; FIXME: we know the http method/version is on the frist line. get it there.
+;; 			(setf line (subseq line 0 (1- (length line))))
+			
+;; 			(cond 
+;; 			  ;; did we find the request line?
+;; 			  ((search " HTTP/1." line)
+;; 			   (setf (slot-value conn 'request-method) (subseq line 0 (search " " line)))
+;; 			   (setf (slot-value conn 'request-uri) 
+;; 					 (subseq line (+ 1 (search " " line)) (search " HTTP/1.1" line)))
+
+;; 			   ;; get the GET request parameters
+;; 			   (let ((uri (slot-value conn 'request-uri)))
+;; 				 (if (search "?" uri)
+;; 					 (let ((key-values (split-by-char 
+;; 										(subseq uri (+ 1 (search "?" uri))) 
+;; 										#\&)))
+;; 					   ;; we have the "x=y" and "z=hello+mom+let's+eat+cheese"
+;; 					   ;; turn those into parameters
+;; 					   (dolist (x key-values)
+;; 						 (let ((key   (subseq x 0 (search "=" x)))
+;; 							   (value (subseq x (+ 1 (search "=" x)) (length x))))
+;; 						   (setf (gethash key (slot-value conn 'get-params)) 
+;; 								 (urldecode value)))))))
+
+;; 			   ) ;; should match up to ((search " HTTP ...
+
+;; 			  ;; do we know? are we in the body?
+;; 			  (in-body
+;; 			   (format t "body: '~a'~%" line))
+
+;; 			  ;; ahh, end of headers
+;; 			  ((string= line "")
+
+;; 			   ;; fixme: clean this up to be something cleaner like (method-is-get conn)
+;; 			   (when (string= "GET" (slot-value conn 'request-method))
+;; 				 (funcall (slot-value conn 'handler) conn)
+;; 				 (return))
+
+;; 			   ;; we'll use this when we support post, etc
+;; 			   ;; not used now
+;; 			   (format t "entering body of request~%")
+;; 			   (setq in-body t)
+;; 			   )
+;; 			  (t
+;; 			   (vector-push-extend line (slot-value conn 'request-headers) )
+;; 			   ))))
+;;       (end-of-file ()
+;; 		(http-networking-cleanup conn)))))
+
+(defun hairball-readline (conn)
+  (with-output-to-string (out)
+	(let ((sock (slot-value conn 'socket))
+		  (buffer (make-array *read-buffer-size*
+							:element-type 'character
+							:adjustable nil
+							:fill-pointer t)))
+	  (setf (fill-pointer buffer) *read-buffer-size*)
+	  (do ((done nil))
+		  (done t)
+		(multiple-value-bind (buf len raddr)
+			(sb-bsd-sockets:socket-receive sock buffer nil)
+		  (format t "Recved ~a bytes" len)
+		  (format t buffer))))))
+  
+
 (defun make-http-responder (conn)
   (lambda (_)
     (declare (ignore _))
-    (handler-case
-		(loop
-		  (let ((line (read-line (slot-value conn 'stream)))
-				(in-body nil))
+	;; get the request and headers...
+	(with-output-to-string (out)
+
+	  (let ((sock (slot-value conn 'socket))
+			(headers (make-array 0 
+								:element-type 'string
+								:fill-pointer 0
+								:adjustable t))
+			(current-header (make-array 0
+										:element-type 'character
+										:fill-pointer 0
+										:adjustable t))
+			(buffer (make-array *read-buffer-size*
+								:element-type 'character
+								:adjustable nil
+								:fill-pointer t)))
+
+		(setf (fill-pointer buffer) *read-buffer-size*)
+		(do ((done nil))
+			(done t)
+		  (multiple-value-bind (buf len raddr)
+			  (sb-bsd-sockets:socket-receive sock buffer nil)
+;;			(declare (ignore raddr))
 			
-			;; FIXME: we know the http method/version is on the frist line. get it there.
-			(setf line (subseq line 0 (1- (length line))))
-			
-			(cond 
-			  ;; did we find the request line?
-			  ((search " HTTP/1." line)
-			   (setf (slot-value conn 'request-method) (subseq line 0 (search " " line)))
-			   (setf (slot-value conn 'request-uri) 
-					 (subseq line (+ 1 (search " " line)) (search " HTTP/1.1" line)))
+			(loop 
+			   for char across buf
+			   do (if (equal #\Newline char)
+					  (let ((trimmed-header (string-right-trim '(#\Space #\Tab #\Newline #\Return #\Linefeed) current-header))) 
+						
+						(if (= 0 (length trimmed-header))
+							(progn 
+							  (format t "DONE!!!!!!~%")
+							  (princ headers)
+							  (return headers)))
+						
+							
+						;; FIXME: there has to be a better way
+						;;        to copy a string onto the array
+						(vector-push-extend (format nil "~a" trimmed-header) headers)
+						
+						(setf (fill-pointer current-header) 0))
+					  (progn 
+						(if (not (equal #\Linefeed char))
+							(vector-push-extend char current-header)))))
 
-			   ;; get the GET request parameters
-			   (let ((uri (slot-value conn 'request-uri)))
-				 (if (search "?" uri)
-					 (let ((key-values (split-by-char 
-										(subseq uri (+ 1 (search "?" uri))) 
-										#\&)))
-					   ;; we have the "x=y" and "z=hello+mom+let's+eat+cheese"
-					   ;; turn those into parameters
-					   (dolist (x key-values)
-						 (let ((key   (subseq x 0 (search "=" x)))
-							   (value (subseq x (+ 1 (search "=" x)) (length x))))
-						   (setf (gethash key (slot-value conn 'get-params)) 
-								 (urldecode value)))))))
+			))))))
 
-			   ) ;; should match up to ((search " HTTP ...
+  
 
-			  ;; do we know? are we in the body?
-			  (in-body
-			   (format t "body: '~a'~%" line))
+  
+;;   (let ((buffer (make-array *read-buffer-size*
+;; 							:element-type 'character
+;; 							:adjustable nil
+;; 							:fill-pointer t))
+;;         (sock (slot-value conn 'socket)))
 
-			  ;; ahh, end of headers
-			  ((string= line "")
+;; 	(do ((fin nil))
+;; 		(fin t)
 
-			   ;; fixme: clean this up to be something cleaner like (method-is-get conn)
-			   (when (string= "GET" (slot-value conn 'request-method))
-				 (funcall (slot-value conn 'handler) conn)
-				 (return))
+;; 	  (setf (fill-pointer buffer) *read-buffer-size*)
+;;       (multiple-value-bind (buf len raddr)
+;;           (sb-bsd-sockets:socket-receive sock buffer nil)
+;;         (declare (ignore raddr))
+;;         (if (null buf)
+;;             (setf fin t)
+;;             (setf (fill-pointer buffer) len)))
+;;       (cond ((= (length buffer) 0)
+;;              (format t "  Got 0 bytes, closing socket and removing handler~%")
+;;              (sb-bsd-sockets:socket-close sock)
+;; ;             (sb-sys:remove-fd-handler (server-session-handler session))
+;;              (setf fin t))
+;;             (fin (format t "Got NIL, returning~%"))
+;;             (t
+;;              (format t "  Read ~a bytes: ~a~%" (length buffer) buffer))))))
 
-			   ;; we'll use this when we support post, etc
-			   ;; not used now
-			   (format t "entering body of request~%")
-			   (setq in-body t)
-			   )
-			  (t
-			   (vector-push-extend line (slot-value conn 'request-headers) )
-			   ))))
-      (end-of-file ()
-		(http-networking-cleanup conn)))))
 
 	  
 (defun serve (conn)
   (let ((stream (sb-bsd-sockets::socket-make-stream (slot-value conn 'socket) 
 				 :output t 
 				 :input t
-				 :element-type *octet*))
+				 :element-type 'character
+				 :buffering :none
+				 ))
+;;				 :element-type *octet*))
 
         (fd (sb-bsd-sockets::socket-file-descriptor (slot-value conn 'socket))))
 
@@ -195,5 +299,6 @@
                                (incf counter)
 ;;                               (format t "Accepted client ~A~%" counter)
                                (serve (make-instance 'http-connection :socket (sb-bsd-sockets::socket-accept socket) :id counter :handler request-handler))))))
+
 
 (provide 'http-server)
